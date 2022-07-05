5459c0a71455962ffce5c84e03aa87d70038147e
Princessdada <princessdada321@gmail.com>
2022-07-05 14:21:32 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 /**
 * print_alphabet_x10 -a function that prints 10 times the alphabet
 *
-* Return: Always 0.
+* Return: Always 0 (Sucess)
 */
 void print_alphabet_x10(void)
 {
 int b, mn;
 
 mn = 0;
+
 while (mn < 10)
 {
 for (b = 'a' ; b <= 'z' ; b++)
