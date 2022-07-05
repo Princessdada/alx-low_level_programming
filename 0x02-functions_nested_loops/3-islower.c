2d837241608baab20d16e5d035584bd67e36cc00
Princessdada <princessdada321@gmail.com>
2022-07-05 15:45:11 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _islower - a function that checks for lower case character
+* @c: single letter input
+* Return: ways 0 (Success)
+*/
+int_islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
