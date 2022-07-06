ffba70350330397279a7dbc94250579c46ee6768
Princessdada <princessdada321@gmail.com>
2022-07-05 20:14:25 -0700

    TASK 8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,8 @@ _putchar(' ');									_putchar(k + '0');
 										
 }
 else if (k >= 10)
-{																				_putchar(',');									_putchar(' ');									_putchar((k / 10) + '0');							_putchar((k % 10) + '0');
+{
+_putchar(',');									_putchar(' ');									_putchar((k / 10) + '0');							_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');									}
