05226f5f8a90d0c4ce568a79fbeca8da0b1f9dd3
Princessdada <princessdada321@gmail.com>
2022-07-05 18:46:43 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,9 +10,9 @@ int i, j, k;
 for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
-										{										k = j * i;
-										if (j == 0)
-																				{																				_putchar(k + '0');								}										if (k < 10 && j != 0)
+										k = j * i;
+										if (j == 0)									{
+_putchar(k + '0');								}										if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
