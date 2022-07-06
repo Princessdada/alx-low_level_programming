62f4817660815db8c388d8e98ccd9820fca0733b
Princessdada <princessdada321@gmail.com>
2022-07-05 17:25:43 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* _abs - a function that prints the absolute value of an integer
+* @ab: integer input
+* Return: Always 0 (Success)
+*/
+int _abs(int ab)
+{
+return (ab * ((ab > 0) - (ab < 0)));
+}
