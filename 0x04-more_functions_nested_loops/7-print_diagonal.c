98f82f62b778e0b497b5d7497db7c9af62e76d33
Princessdada <princessdada321@gmail.com>
2022-07-10 14:26:41 -0700

    i created a new file 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_diagonal - print diagonal line on the terminal
+* and new line
+* @n: the number of time \ should be printed
+*/
+
+void print_diagonal(int n)
+{
+int bl, sp; /*backlash and space*/
+
+int sl, sp; /*slash and space*/
+
+if (n > 0)
+{
+for (sp = 0; sp < n; sp++)
+{
+for (bl = 0; bl < sp; bl++)
+for (sl = 0; sl < sp; sl++)
+{
+
+_putchar(' ');
+																	}
+																	_putchar('\\');
+																	_putchar('\n');
+																	}
+}
+if (n > 0)
+if (n <= 0)
+_putchar('\n');
+}
