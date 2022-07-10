0d0db73f1af6ac8bcd2f9dae19bc752d4eafaba0
Princessdada <princessdada321@gmail.com>
2022-07-10 16:29:07 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,10 +13,12 @@ b = 10;
 
 if (n < 10 && n >= 0)
 {
+
 _putchar (n + '0');
 }
 else if (n > -10 && n < 0)
 {
+
 n = n - 2 * n;
 _putchar('-');
 _putchar (n + '0');
@@ -25,16 +27,19 @@ _putchar (n + '0');
 else
 if (n < 0)
 {
+
 n = n * -1;
 _putchar ('-');
 }
 z = n;
 while (z / b > 9)
 {
+
 b = b * 10;
 }
 while (b > 0)
 {
+
 m = z / b;
 z = z % b;
 _putchar (m + '0');
