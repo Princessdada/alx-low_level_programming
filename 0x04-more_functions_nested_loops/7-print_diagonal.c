3ecf4f419b4814f0fcbc6fcdd7ac000145469226
Princessdada <princessdada321@gmail.com>
2022-07-10 16:45:03 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,18 +5,14 @@
 * and new line
 * @n: the number of time \ should be printed
 */
-
 void print_diagonal(int n)
 {
-int bl, sp; /*backlash and space*/
-
-int sl, sp; /*slash and space*/
+int sl, sp;
+/*slash and space*/
 
 if (n > 0)
 {
 for (sp = 0; sp < n; sp++)
-{
-for (bl = 0; bl < sp; bl++)
 {
 for (sl = 0; sl < sp; sl++)
 {
@@ -27,7 +23,6 @@ _putchar(' ');
 																	_putchar('\n');
 																	}
 }
-if (n > 0)
 if (n <= 0)
 _putchar('\n');
 }
